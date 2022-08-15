7d17876b1b96892e2b6109eaf6e2b0d7140506f1
macdonst <simon.macdonald@gmail.com>
2022-08-14 22:30:27 -0400

    Only output attributes that are set
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- packages/text-input/TextInput.js
+++ packages/text-input/TextInput.js
@@ -1,63 +1,64 @@
 export default function TextInput({ html, state }) {
-    const { attrs={} } = state
-    const {
-      description='',
-      form='',
-      label='',
-      max='',
-      maxlength='',
-      min='',
-      minlength='',
-      name='',
-      pattern='',
-      placeholder='',
-      size='',
-      step='',
-      value='',
-      type='',
-    } = attrs
+  const { attrs={} } = state
+  const {
+    description='',
+    form='',
+    id='',
+    label='',
+    max='',
+    maxlength='',
+    min='',
+    minlength='',
+    name='',
+    pattern='',
+    placeholder='',
+    size='',
+    step='',
+    value='',
+    type='',
+  } = attrs
 
-    const booleanAttr = (attrs, attr) => Object.keys(attrs).includes(attr) ? attr : ''
+  const booleanAttr = (attrs, attr) => Object.keys(attrs).includes(attr) ? attr : ''
 
-    const autocomplete = booleanAttr(attrs, 'autocomplete')
-    const autofocus = booleanAttr(attrs, 'autofocus')
-    const disabled = booleanAttr(attrs, 'disabled')
-    const readonly = booleanAttr(attrs, 'readonly')
-    const required = booleanAttr(attrs, 'required')
+  const autocomplete = booleanAttr(attrs, 'autocomplete')
+  const autofocus = booleanAttr(attrs, 'autofocus')
+  const disabled = booleanAttr(attrs, 'disabled')
+  const readonly = booleanAttr(attrs, 'readonly')
+  const required = booleanAttr(attrs, 'required')
 
-    return html`
-  <label
-    for="${name}"
-    class="
-     radius1
-    "
-  >
-    <p class="pb-2">
-      ${label}
-    </p>
-    <input
-      class="p-3 mb-2"
-      form="${form}"
-      max="${max}"
-      maxlength="${maxlength}"
-      min="${min}"
-      minlength="${minlength}"
-      name="${name}"
-      pattern="${pattern}"
-      placeholder="${placeholder}"
-      size="${size}"
-      step="${step}"
-      type="${type}"
-      value="${value}"
-      ${autocomplete}
-      ${autofocus}
-      ${disabled}
-      ${readonly}
-      ${required}
-     >
-    <p>
-     ${description}
-    </p>
-  </label>
-    `
-  }
+  return html`
+<label
+  for="${name}"
+  class="
+   radius1
+  "
+>
+  <p class="pb-2">
+    ${label}
+  </p>
+  <input
+    class="p-3 mb-2"
+    ${form ? `form="${form}"` : ''}
+    ${id ? `id="${id}"` : ''}
+    ${max ? `max="${max}"` : ''}
+    ${maxlength ? `maxlength="${maxlength}"` : ''}
+    ${min ? `min="${min}"` : ''}
+    ${minlength ? `minlength="${minlength}"` : ''}
+    ${pattern ? `pattern="${pattern}"` : ''}
+    ${placeholder ? `placeholder="${placeholder}"` : ''}
+    ${size ? `size="${size}"` : ''}
+    ${step ? `step="${step}"` : ''}
+    ${type ? `type="${type}"` : ''}
+    ${value ? `value="${value}"` : ''}
+    ${autocomplete ? 'autocomplete' : ''}
+    ${autofocus ? 'autofocus' : ''}
+    ${disabled ? 'disabled' : ''}
+    ${readonly ? 'readonly' : ''}
+    ${required ? 'required' : ''}
+   >
+  ${description ? `<p>
+${description}
+</p>` : ''}
+</label>
+  `
+}
