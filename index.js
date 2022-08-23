f86148ac2a03a720b2f6a418782759946c96eaee
macdonst <simon.macdonald@gmail.com>
2022-08-22 22:34:08 -0400

    Add link element
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import LinkElement from './packages/link-element/LinkElement.js'
 import FieldSet from './packages/field-set/FieldSet.js'
 import FormElement from './packages/form-element/FormElement.js'
 import PageContainer from './packages/page-container/PageContainer.js'
@@ -7,6 +8,7 @@ import TextInput from './packages/text-input/TextInput.js'
 export {
     FieldSet,
     FormElement,
+    LinkElement,
     PageContainer,
     SubmitButton,
     TextInput
