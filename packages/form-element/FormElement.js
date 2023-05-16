c72a93b91056d3e40fe1d25013813e5b8a41dffa
kj <kj@begin.com>
2023-05-16 16:27:56 -0700

    Removes fixed widths for form element

--- packages/form-element/FormElement.js
+++ packages/form-element/FormElement.js
@@ -3,13 +3,10 @@ export default function FormElement({ html, state }) {
     const { action = '', method = '' } = attrs
 
     return html`
-<style>
-    :host form {
-        min-width: 20rem;
-        max-width: 40rem;
-    }
-</style>
-<form class="flex flex-col gap-1 m-auto" action="${action}" method="${method}">
+<form
+  action="${action}"
+  method="${method}"
+>
   <slot></slot>
 </form>
 `
